1e895b40469d268bb149e08cdd62fb42a83daee6
Ted <cespedesmonserra4@gmail.com>
2021-01-14 15:30:32 -0600

    Cambios finales

--- App.py
+++ App.py
@@ -26,7 +26,7 @@ def Login():
             return render_template('login.html')
 
     elif request.method == "POST":
-        #try:
+        try:
             usuario = request.form['usuario']
             password = request.form['password']
 
@@ -41,9 +41,9 @@ def Login():
             else:
                 flash("Datos incorrectos")
                 return redirect(url_for('Login'))
-        # except:
-        #     flash("Error")
-        #     return redirect(url_for('Login')) 
+        except:
+             flash("Error")
+             return redirect(url_for('Login')) 
             
 @app.route('/logout', methods = ['POST'])
 def Logout():
